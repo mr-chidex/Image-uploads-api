33eca95975495c6e1ad5a0727fc063fd3859e477
mr-chidex <mbahchiboy2@gmail.com>
2021-02-25 16:27:39 +0100

    added validation

--- controllers/image.js
+++ controllers/image.js
@@ -1,8 +1,17 @@
+const Image = require("../models/image");
+const cloudinary = require("../handler/cloudinary");
+
 //@desc  Upload image to cloud
 //@Route    POST/api/images
 //@Access   Public (Note: In your projects this should be private)
 exports.addImage = async (req, res, next) => {
   try {
+    if (!req.file)
+      return res.status(400).json({ message: "please select an image" });
+    console.log(req.file);
+
+    const cloudImage = await cloudinary.v2.uploader.upload(req.file);
+    console.log("cloud_image", cloudImage);
   } catch (error) {
     next(error);
   }
