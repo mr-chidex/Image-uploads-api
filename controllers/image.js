8596a405273dd25b099875ece475832e037040fa
mr-chidex <mbahchiboy2@gmail.com>
2021-02-25 14:00:09 +0100

    setting up image controller

---
+++ controllers/image.js
@@ -0,0 +1,19 @@
+//@desc  Upload image to cloud
+//@Route    POST/api/images
+//@Access   Public (Note: In your projects this should be private)
+exports.addImage = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+//@desc  Delete uploaded image from cloud
+//@Route    DELETE/api/images
+//@Access   Public (Note: In your projects this should be private)
+exports.deleteImage = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
