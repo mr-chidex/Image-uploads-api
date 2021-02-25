e41d109dcfa84c1c38ff39b73eb112257e1b2d99
mr-chidex <mbahchiboy2@gmail.com>
2021-02-25 15:47:15 +0100

    setting up cloudinary and multer

---
+++ middleware/multer.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+
+const imageFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const imageStorage = multer.diskStorage({});
+
+module.exports = multer({
+  storage: imageStorage,
+  fileFilter: imageFilter,
+  limits: { fileSize: 500000 },
+});
